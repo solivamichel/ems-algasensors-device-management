a9fa1d33fcfa429557a46618ace8acc670cecdea
Michel Soliva <michelsolivadev@gmail.com>
2025-06-03 19:06:22 -0300

    New projects files

---
+++ src/test/java/com/soliva/algasensors/device/management/DeviceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soliva.algasensors.device.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
